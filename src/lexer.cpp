c03918b5d0ca14b46e86847ee74fbc5dc4048649
InfernalKn1ght <arzhaev.dmitry@yandex.ru>
2025-01-06 21:43:45 +0300

    refactor(lexer): code restructuring

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,107 +1,128 @@
 #include "lexer.h"
 #include <iostream>
-// TODO: все таки переписать с unique_ptr
-void Lexer::reserve(Word word) {
-    word_table.emplace(word.lexeme, word);
+
+bool Lexer::lexer_eof() {
+    return _reader.reader_eof();
 }
 
-bool Lexer::read_char() {
-    return reader.read_symbol(peek);
+void Lexer::read_peek() {
+    _reader.read_symbol(_peek);
 }
 
 bool Lexer::next_char(const char &c) {
-    if (!read_char() || c != peek) {
+    if (c != _peek) {
         return 0;
     }
-    peek = ' ';
+    _peek = ' ';
     return 1;
 }
 
 void Lexer::skip_whitespace() {
-    do {
-        if (peek == '\n') {
-            ++line;
-            continue;
-        }
-        if (peek != ' ' && peek != '\t') {
+    while (!lexer_eof()) {
+        if (_peek != ' ' && _peek != '\t' && _peek != '\n') {
             break;
         }
-    } while (read_char());
+        if (_peek == '\n') {
+            ++line;
+        }
+        read_peek();
+    }
 }
 
 void Lexer::acc_number(std::string &acc) {
     do {
-        acc.push_back(peek);
-    } while (read_char() && std::isdigit(peek));
+        acc.push_back(_peek);
+        read_peek();
+    } while (!lexer_eof() && std::isdigit(_peek));
+}
+
+void Lexer::acc_word(std::string &acc) {
+    do {
+        acc.push_back(_peek);
+        read_peek();
+    } while (!lexer_eof() && is_alpha_or_digit(_peek));
 }
 
 bool Lexer::is_alpha_or_digit(char &c) {
     return std::isdigit(c) || std::isalpha(c);
 }
 
+std::unique_ptr<Token> Lexer::make_const_token() {
+    std::string acc;
+    acc_number(acc);
+    if (_peek == '.') {
+        acc_number(acc);
+        return std::make_unique<Real>(std::stod(acc));
+    }
+    return std::make_unique<Num>(std::stoi(acc));
+}
+
+std::unique_ptr<Token> Lexer::make_word_token() {
+    std::string acc;
+    acc_word(acc);
+    if (_word_table.contains(acc)) {
+        return std::make_unique<Word>(_word_table[acc]);
+    }
+    return std::make_unique<Word>(Tag::ID, acc);
+}
+
+std::unique_ptr<Token> Lexer::make_char_token() {
+    char chtag = _peek;
+    _peek = ' ';
+    return std::make_unique<Token>(chtag);
+}
+
+std::unique_ptr<Token> Lexer::make_token() {
+    if (std::isdigit(_peek)) {
+        return make_const_token();
+    } else if (std::isalpha(_peek)) {
+        return make_word_token();
+    } else {
+        return make_char_token();
+    }
+}
+
 std::unique_ptr<Token> Lexer::scan() {
     skip_whitespace();
 
-    switch (peek) {
+    switch (_peek) {
     case '&':
         if (next_char('&')) {
-            return std::make_unique<Word>(word_table["&&"]);
+            return std::make_unique<Word>(_word_table["&&"]);
         }
         return std::make_unique<Token>('&');
     case '|':
         if (next_char('|')) {
-            return std::make_unique<Word>(word_table["||"]);
+            return std::make_unique<Word>(_word_table["||"]);
         }
         return std::make_unique<Token>('|');
     case '<':
         if (next_char('=')) {
-            return std::make_unique<Word>(word_table["<="]);
+            return std::make_unique<Word>(_word_table["<="]);
         }
         return std::make_unique<Token>('<');
     case '>':
         if (next_char('=')) {
-            return std::make_unique<Word>(word_table[">="]);
+            return std::make_unique<Word>(_word_table[">="]);
         }
         return std::make_unique<Token>('>');
     case '=':
         if (next_char('=')) {
-            return std::make_unique<Word>(word_table["=="]);
+            return std::make_unique<Word>(_word_table["=="]);
         }
         return std::make_unique<Token>('=');
 
     default:
-        break;
+        return make_token();
     }
-
-    std::string acc;
-    if (std::isdigit(peek)) {
-        acc_number(acc);
-        if (peek == '.') {
-            acc.push_back('.');
-            if (read_char()) { // TODO: Error
-                acc_number(acc);
-                return std::make_unique<Real>(std::stod(acc));
-            }
-        }
-        return std::make_unique<Num>(std::stoi(acc));
-    }
-    if (std::isalpha(peek)) {
-        do {
-            acc.push_back(peek);
-        } while (read_char() && is_alpha_or_digit(peek));
-
-        if (word_table.contains(acc)) {
-            return std::make_unique<Word>(word_table[acc]);
-        }
-        return std::make_unique<Word>(Tag::ID, acc);
-    }
-    char chtag = peek;
-    peek = ' ';
-    return std::make_unique<Token>(chtag);
 }
 
-Lexer::Lexer(const std::string &file_name) : reader(file_name) { // Здесь резервируются ключевые слова в хэш-таблице
-    reserve(Word(Tag::AND, "&&"));                               // TODO: позже добавить все оставшиеся ключевые слова
+void Lexer::reserve(Word word) {
+    _word_table.emplace(word.lexeme, word);
+}
+
+Lexer::Lexer(const std::string &file_name) : _reader(file_name) {
+    reserve(Word(Tag::AND, "&&"));
     reserve(Word(Tag::OR, "||"));
     reserve(Word(Tag::LE, "<="));
     reserve(Word(Tag::GE, ">="));
--- src/lexer.h
+++ src/lexer.h
@@ -5,21 +5,27 @@
 #include <memory>
 #include <unordered_map>
 
-class Lexer {
-private:
-    Reader reader;
-    char peek = ' ';
-    std::unordered_map<std::string, Word> word_table;
-    void reserve(Word word);
-    void skip_whitespace();
-    bool read_char();
-    bool next_char(const char &c);
-    void acc_number(std::string &acc);
-    bool is_alpha_or_digit(char &c);
-
+class Lexer { // TODO: написать комментарии
 public:
     unsigned int line = 0;
-
     std::unique_ptr<Token> scan();
+    bool lexer_eof();
     Lexer(const std::string &file_name);
+    ~Lexer() = default;
+
+private:
+    Reader _reader;
+    char _peek = ' ';
+    std::unordered_map<std::string, Word> _word_table;
+    void read_peek();
+    bool next_char(const char &c);
+    void skip_whitespace();
+    void acc_number(std::string &acc);
+    void acc_word(std::string &acc);
+    bool is_alpha_or_digit(char &c);
+    std::unique_ptr<Token> make_const_token();
+    std::unique_ptr<Token> make_word_token();
+    std::unique_ptr<Token> make_char_token();
+    std::unique_ptr<Token> make_token();
+    void reserve(Word word);
 };
